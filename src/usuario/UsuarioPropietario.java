cc4a475afa5a49bfbe046175af26f9e4c096eca0
Leo <leodelgadodev@gmail.com>
2019-10-08 15:24:56 -0300

    Primer commit

--- src/usuario/UsuarioPropietario.java
+++
@@ -1,5 +0,0 @@
-package usuario;
-
-public class UsuarioPropietario {
-
-}
---
+++ unq-tp-obj2/src/usuario/UsuarioPropietario.java
@@ -0,0 +1,11 @@
+package usuario;
+
+import org.junit.jupiter.api.Test;
+
+public class UsuarioPropietario {
+
+	@Test
+	public void testPropietarioDebeEstarRegistradoEnElSitioParaPublicar() {
+		
+	}
+}
