e3e843f389ccd808979ae73a811c1fa039158668
leo <leo@leo>
2019-10-11 23:31:03 -0300

    Corregidos test de Usuario

--- unq-tp-obj2/src/clasesTest/AdministradorTest.java
+++ unq-tp-obj2/src/clasesTest/AdministradorTest.java
@@ -1,5 +1,35 @@
 package clasesTest;
 
+import org.junit.Test;
+
 public class AdministradorTest {
 
+	// "Dar de alta" siginifca agregar
+	
+	@Test
+	public void darDeAltaCategoriasUtilizadasParaRankearPropietarios() {
+		
+	}
+	
+	@Test
+	public void darDeAltaCategoriasUtilizadasParaRankearInquilinos() {
+		
+	}
+	
+	@Test
+	public void darDeAltaCategoriasUtilizadasParaRankearInmuebles() {
+		
+	}
+	
+	@Test
+	public void darDeAltaTiposDeInmuebles() {
+		
+	}
+	
+	@Test
+	public void darDeAltaServiciosDeInmuebles() {
+		
+	}
+	
+	
 }
--- unq-tp-obj2/src/usuarioTest/UsuarioTest.java
+++ unq-tp-obj2/src/usuarioTest/UsuarioTest.java
@@ -10,47 +10,55 @@ import usuario.UsuarioPropietario;
 
 public class UsuarioTest {
 
-	Usuario user1 = new UsuarioInquilino("Aaa", "a@email.com", 1111);
-	Usuario user2 = new UsuarioPropietario("Aaa", "a@email.com", 1111);
-	
+
 	@Test
 	public void testConstructorUsuarioInquilino() {
-		
+		Usuario user1 = new UsuarioInquilino("Aaa", "Aaa", "a@email.com", 1111);
 		
 		assertNotNull(user1.getNombre());
+		assertNotNull(user1.getApellido());
 		assertNotNull(user1.getEmail());
 		assertNotNull(user1.getTelefono());
 	}
 	
 	@Test
 	public void testConstructorUsuarioPropietario() {
-		
+		Usuario user2 = new UsuarioPropietario("Aaa", "Aaa", "a@email.com", 1111);
 		
 		assertNotNull(user2.getNombre());
+		assertNotNull(user2.getApellido());
 		assertNotNull(user2.getEmail());
 		assertNotNull(user2.getTelefono());
 	}
 	
 	@Test
 	public void testSettersUsuarioInquilino() {
-		user1.setNombre("bbb");
-		user1.setEmail("b@email.com");
-		user1.setTelefono(2222);
+		Usuario user = new UsuarioInquilino(null, null, null, null);
 		
-		assertNotNull(user1.getNombre());
-		assertNotNull(user1.getEmail());
-		assertNotNull(user1.getTelefono());
+		user.setNombre("bbb");
+		user.setApellido("bbb");
+		user.setEmail("b@email.com");
+		user.setTelefono(2222);
+		
+		assertNotNull(user.getNombre());
+		assertNotNull(user.getApellido());
+		assertNotNull(user.getEmail());
+		assertNotNull(user.getTelefono());
 	}
 	
 	@Test
 	public void testSettersUsuarioPropietario() {
-		user2.setNombre("bbb");
-		user2.setEmail("b@email.com");
-		user2.setTelefono(2222);
+		Usuario user = new UsuarioPropietario(null, null, null, null);
 		
-		assertNotNull(user2.getNombre());
-		assertNotNull(user2.getEmail());
-		assertNotNull(user2.getTelefono());
+		user.setNombre("bbb");
+		user.setApellido("bbb");
+		user.setEmail("b@email.com");
+		user.setTelefono(2222);
+		
+		assertNotNull(user.getNombre());
+		assertNotNull(user.getApellido());
+		assertNotNull(user.getEmail());
+		assertNotNull(user.getTelefono());
 	}
 	
 	
