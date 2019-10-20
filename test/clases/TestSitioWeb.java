432d1eb208433c2f82839efddeb9f7802747b078
gonzabl <gonzabl@live.com.ar>
2019-10-20 19:04:54 -0300

    test web x2

--- test/clases/TestSitioWeb.java
+++ test/clases/TestSitioWeb.java
@@ -28,17 +28,13 @@ class TestSitioWeb {
 	ArrayList<String> a = new ArrayList<>();
 	Set<String> servicios = new HashSet<String>(); 
 	
-	Usuario prop1 = new Usuario("Leo","Delgado", "leo@email.com", 1526248982);
-	Usuario prop2 = new Usuario("Gonza","Torrez", "gonza@email.com", 1585248596);
-	Usuario inquilino = new Usuario("Daniel", "Cross","cross@gmail.com",1553986574);
-	
-	Inmueble casa1 = new Inmueble(prop1, "Casa", "BsAs", "Argentina","calle 123" , 
+	Inmueble casa1 = new Inmueble(mock(Usuario.class), "Casa", "BsAs", "Argentina","calle 123" , 
 			servicios, 5,"2019-01-01","2019-01-30", "08:30", "17:00", 2500.0);
 	
-	Inmueble casa2 = new Inmueble(prop1, "Casa", "Cordoba", "Argentina","calle 53" , 
+	Inmueble casa2 = new Inmueble(mock(Usuario.class), "Casa", "Cordoba", "Argentina","calle 53" , 
 			servicios, 10,"2019-06-01","2019-06-20", "09:30", "19:00", 3500.0);
 	
-	Inmueble casa3 = new Inmueble(prop2, "Casa", "BsAs", "Argentina","calle 18" , 
+	Inmueble casa3 = new Inmueble(mock(Usuario.class), "Casa", "BsAs", "Argentina","calle 18" , 
 			servicios, 3,"2019-01-01","2019-01-20", "10:30", "18:00", 4000.0);
 	
 	@BeforeEach
@@ -142,8 +138,8 @@ class TestSitioWeb {
 	public void testBuscarInmueblesPorCiudadYFechas2() {
 		
 		List<Inmueble> resultado = new ArrayList<Inmueble>();
-		resultado.add(casa1);
 		resultado.add(casa3);
+		resultado.add(casa1);
 		
 		assertEquals(resultado,web.buscarInmuebles("BsAs", "2019-01-10", "2019-01-14"));
 	}
